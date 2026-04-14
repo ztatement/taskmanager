3f92d2ccd6eb780c7d136b171f8ef142111f71a4
Thomas <ztatements@gmail.com>
2026-04-14 11:41:10 +0200

    Autoload-Datei für die TaskManager-Anwendung

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,7 +1,18 @@
 <?php
+
+  declare(strict_types=1);
+
 /**
   * Autoload-Datei für die TaskManager-Anwendung
+  *
+  * @author Thomas Boettcher @ztatement (github[at]ztatement[dot]com)
+  * @copyright (c) 2026 ztatement
   * 
+  * @version 1.0.0.2026.03.24
+  * @file $Id: autoload.php $
+  * @created $Id: 1 Donnerstag, 12. Februar 2026, 15:15:03 GMT+0200Z ztatement $
+  * 
+  * @description Autoload-Datei für die TaskManager-Anwendung
   * Diese Datei wird am Anfang jeder Seite geladen und initialisiert:
   * - Session-Sicherheit
   * - Datenbank-Verbindung
@@ -9,22 +20,10 @@
   * - Sprach-System
   * - Zentrale Klassen-Instanzen
   *
-  * @author Thomas Boettcher <github[at]ztatement[dot]com>
-  * @copyright (c) 2026 ztatement
-  * 
-  * @version 1.0.0.2026.03.24
-  * @file $Id: autoload.php 1 Donnerstag, 12. Februar 2026, 15:15:03 GMT+0200Z ztatement $
-  * 
-  * @link https://github.com/ztatement/taskmanager
-  * 
-  * @license MIT
-  *
-  * @category Initialization
-  * @package TaskManager
+  * @repository https://github.com/ztatement/taskmanager
+  * @license MIT (https://opensource.org/license/MIT)
   */
 
-  declare(strict_types=1);
-
   // Konfiguration laden (wird von vielen Klassen benötigt)
-  require_once __DIR__ . '/config.php';
-  require_once BASE_PATH . '/classes/Autoloader.php';
+  require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
+  require_once BASE_PATH . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'Autoloader.php';
